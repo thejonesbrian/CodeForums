a725bc4c62815bfa65281cf883eff03d9b0746fc
Brian Jones <brijones.nz@gmail.com>
2020-06-30 20:42:08 +1200

    Add project files.

---
+++ CodeForums/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeForums.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
