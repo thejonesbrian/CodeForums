b2b8445b578aa9099901f9bd69fce81d41a13a6f
Brian Jones <brijones.nz@gmail.com>
2020-07-02 23:04:02 +1200

    Add service layer

---
+++ CodeForums.Service/ForumService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodeForums.Service
+{
+    public class ForumService
+    {
+    }
+}
---
+++ CodeForums/Data/IForum.cs
@@ -0,0 +1,20 @@
+﻿using CodeForums.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeForums.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GatAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
