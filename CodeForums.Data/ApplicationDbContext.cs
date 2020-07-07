ed20792e752520126c587116fd81ffb5f9def2ed
Brian Jones <brijones.nz@gmail.com>
2020-07-07 16:52:33 +1200

    Initial commit version 2, create data proj

---
+++ CodeForums.Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CodeForums.Data.Models;
+using CodeForums.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeForums.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+    }
+}
---
+++ CodeForums.Data/Models/PostReply.cs
@@ -0,0 +1,15 @@
+﻿using CodeForums.Models;
+using System;
+
+namespace CodeForums.Data.Models
+{
+    public class PostReply
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
